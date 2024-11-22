50d25238f305bad4eda5480ec5444b994b7b8e6d
ZiwenZhang <a60595677@gmail.com>
2024-11-22 12:13:39 -0500

    update file name and migration

--- react-app/src/components/HomePage/StockSideBar.jsx
+++ react-app/src/components/HomePage/StockSideBar.jsx
@@ -15,15 +15,15 @@ function StockSideBar() {
   }
 
   return (
-    <div className="p-4 bg-gray-800 text-white shadow-md w-1/4 max-h-screen overflow-y-auto text-left"> {/* 调整宽度和高度 */}
+    <div className="p-4 bg-gray-800 text-white shadow-md w-1/5 max-h-screen overflow-y-auto text-left scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800 hover:scrollbar-thumb-gray-400">
       <h2 className="text-xl text-center font-bold mb-4 text-yellow-400">Stock List</h2>
-      <div className="space-y-4 scrollbar scrollbar-thumb-gray-700 scrollbar-track-gray-800 scrollbar-thin">
+      <div className="space-y-4">
         {stocks.map((stock) => (
           <div
             key={stock.id}
             className="p-4 bg-gray-900 rounded-md shadow-md hover:bg-gray-700 cursor-pointer"
           >
-            <div className="flex items-center justify-between"> {/* 居中对齐 */}
+            <div className="flex items-center justify-between">
               <div>
                 <p className="text-lg font-semibold">{stock.name}</p>
                 <p className="text-sm text-gray-400">{stock.symbol}</p>
