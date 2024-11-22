5ff8c422dca3267294d1dc1387096eafe4fe5685
ZiwenZhang <a60595677@gmail.com>
2024-11-22 17:33:03 -0500

    a

--- app/__init__.py
+++ app/__init__.py
@@ -49,10 +49,6 @@ CORS(app)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-
 
 @socketio.on("connect")
 def handle_connect():
