2cb0c7f323773a2c25abc14ca1512b93f9f6b5bb
ZiwenZhang <a60595677@gmail.com>
2024-11-25 01:26:18 -0500

    deploytest

--- app/sockets/save_notification.py
+++ app/sockets/save_notification.py
@@ -1,32 +1,32 @@
-from time import sleep
-from app.models import Save, Stock
-from flask_socketio import emit
+# from time import sleep
+# from app.models import Save, Stock
+# from flask_socketio import emit
 
-def process_save_notifications(socketio):
-    from app import app ,db
-    while True:
-        try:
-            with app.app_context():
-                saves = Save.query.all()
-                for save in saves:
-                    stock = Stock.query.get(save.stock_id)
-                    if not stock:
-                        continue
+# def process_save_notifications(socketio):
+#     from app import app ,db
+#     while True:
+#         try:
+#             with app.app_context():
+#                 saves = Save.query.all()
+#                 for save in saves:
+#                     stock = Stock.query.get(save.stock_id)
+#                     if not stock:
+#                         continue
 
-                    if stock.price >= save.target_price:
-                        message = f"Stock {stock.name} has reached your target price of {save.target_price}!"
+#                     if stock.price >= save.target_price:
+#                         message = f"Stock {stock.name} has reached your target price of {save.target_price}!"
 
-                        socketio.emit(
-                            "save_notification",
-                            {"user_id": save.user_id, "message": message},
-                            to="/"
-                        )
-                        print(f"[WebSocket] Sent notification to User {save.user_id}: {message}")
+#                         socketio.emit(
+#                             "save_notification",
+#                             {"user_id": save.user_id, "message": message},
+#                             to="/"
+#                         )
+#                         print(f"[WebSocket] Sent notification to User {save.user_id}: {message}")
                         
-                        # db.session.delete(save)
-                        db.session.commit()
-                        return 
-            sleep(5)
-        except Exception as e:
-            print(f"Error in process_save_notifications: {e}")
-            sleep(5)
+#                         # db.session.delete(save)
+#                         db.session.commit()
+#                         return 
+#             sleep(5)
+#         except Exception as e:
+#             print(f"Error in process_save_notifications: {e}")
+#             sleep(5)
